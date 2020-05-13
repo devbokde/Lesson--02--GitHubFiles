d4559e582fac7c17463426a42e8ed3b6384ba3fa
Ubuntu <devbokderediffm@devbokderediffm.z3b0ebzjah4ufc1zv433t5225e.gx.internal.cloudapp.net>
2020-05-13 10:34:39 +0000

    i am pushing all the files to github

---
+++ HelloJava.java
@@ -0,0 +1,12 @@
+ss HelloJava {
+
+public static void main(String args[]){
+
+System.out.Println(“ I am your Java Program. Thank you! ”);
+
+}
+
+}
+
+his is my first editor file of java in github practise . Thank you.
+Dated: 13 May 2020
